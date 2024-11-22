133abf08df1899d709f8e2a25ed64a141055e22f
Leonardo Larrañaga <leonardolarranaga@icloud.com>
2024-11-21 21:09:46 -0800

    Initial Commit

---
+++ app/src/main/java/studio/leonardolarranaga/tvshows/presentation/TVShowsApp.kt
@@ -0,0 +1,9 @@
+package studio.leonardolarranaga.tvshows.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TVShowsApp(modifier: Modifier = Modifier) {
+
+}
